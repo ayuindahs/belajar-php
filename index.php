00f6115b6d6d2c2ee7af8f8df7659efd32ba703d
Ayu Indahsari <ayu62539@gmail.com>
2022-10-24 14:58:15 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Ayu Indahsari";
+$umur =  21;
+
+echo "Nama saya <strong>$nama</strong>, Saya berusia $umur tahun.<br>";
+
+$namaKakak = "Ahmad Nur Ali";
+$umurKakak = 22;
+
+echo "Nama kakak saya <strong>$namaKakak</strong>, Kakak saya berusia $umurKakak tahun.<br>";
+
+$selisihumur = $umurKakak - $umur;
+echo "Selisih usia saya dan kakak saya adalah $selisihumur tahun.";
+
+
+?>
